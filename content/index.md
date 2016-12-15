e2d568b827a11accb2f4cd9e78b678fcc47b2754
Marten Veldthuis <marten@veldthuis.com>
2016-12-15 10:08:18 +0000

    init

---
+++ content/index.md
@@ -0,0 +1,7 @@
++++
+date = "2016-12-12T17:06:36Z"
+draft = false
+title = "index"
+
++++
+
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hugo -s . --theme ellen -d ellen-marten.nl
+hugo -s . --theme marten -d marten-ellen.nl
